7fd06ca466ebedc69b3e690ebc481b6d64b81bc4
naeemalomari <naeemalomari96@gmail.com>
2021-09-28 23:08:38 +0300

    lab3

---
+++ linter/app/src/test/java/linter/Main.java
@@ -0,0 +1,8 @@
+//package linter;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("ichrricbcurbcrc");
+//        App app =new App();
+//    }
+//}
