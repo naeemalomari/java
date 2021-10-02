e0445432708d80536edbe9c61e90c84290515aa8
naeemalomari <naeemalomari96@gmail.com>
2021-10-02 17:01:11 +0300

    changes

--- linter/app/src/test/java/linter/Main.java
+++ linter/app/src/test/java/linter/Main.java
@@ -5,4 +5,5 @@ public class Main {
     public static void main(String[] args) {
         App.readFile("C:\\Users\\Student\\401Java\\java-fundemantals\\linter\\app\\src\\main\\resources\\gates.js");
     }
+
 }
